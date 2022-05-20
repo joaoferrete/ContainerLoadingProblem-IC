300a8969a8107eca5886bfadc46abf4c24af46ec
João Ferrete <jpferreteg@gmail.com>
2022-05-20 01:07:39 -0300

    Modelo 2: Sem rotação + padrao input

---
+++ 02 - Entrada Melhorada - Sem Rotacao/Util.h
@@ -0,0 +1,57 @@
+#ifndef ___UTIL_H__
+#define ___UTIL_H__
+
+
+
+#include <bits/stdc++.h>
+#include <GL/glut.h>
+#include <time.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <ilcplex/ilocplex.h>
+
+using namespace std;
+
+
+
+typedef IloArray<IloNumVarArray> NumVarMatrix2D;
+typedef IloArray<NumVarMatrix2D> NumVarMatrix3D;
+typedef IloArray<IloExprArray>   ExprMatrix;
+typedef IloArray<IloBoolVarArray> BoolVarMatrix;
+typedef unsigned long long llu;
+
+
+
+//==============================================================================
+//                      ESTRUTURAS
+//==============================================================================
+typedef struct {
+    double x, y, z;  // Coordenadas
+    double tx, ty, tz;   // Tamanho
+    double r, g, b;  // Cor
+    int id;  // Identificador
+} Caixa;
+
+
+
+
+//==============================================================================
+//                        VARIÁVEIS GLOBAIS    
+//==============================================================================
+float rotate_x = -22, rotate_y = 130;   //Variáveis de rotação
+float x, y, z;                      //Posição do container
+size_t totalCx = 0;                 //Total de caixas
+
+
+
+
+
+//==============================================================================
+//                        FUNÇÕES AUXILIARES
+//==============================================================================
+
+bool ordena(Caixa a, Caixa b);
+
+
+
+#endif
